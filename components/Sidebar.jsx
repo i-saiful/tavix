5d0110fe9f071e672463b805348fd8395d3a2403
Saiful Islam <mdsaiful.islam@bjitgroup.com>
2026-09-09 16:08:20 +0600

    feat: initial Sidebar implementation
    - Add basic Sidebar component with navigation structure and layout

---
+++ components/Sidebar.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Sidebar } from "tavix";
+import Link from "next/link";
+import sidebar from "@/content/data/sidebar.json";
+
+export default function AppSidebar() {
+  return <Sidebar navItems={sidebar} Link={Link} />;
+}
